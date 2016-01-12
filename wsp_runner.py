bcd95b032ddeb0af43746dacc08df34e20f328eb
Perry Bhandal <perry.bhandal@gmail.com>
2016-01-12 14:26:34 -0500

    Completed typing, key presses and mouse movements.

--- wsp_runner.py
+++ wsp_runner.py
@@ -1,9 +1,20 @@
 import os
 import sys
 import time
+import subprocess
 
 PREP_SCRIPT_NAME = "wsprepare.py"
 SLEEP_TIME = 1 # Time in seconds to sleep after a command, mouse move or key press.
+
+SNAP_MAPS = {"left": "alt+left",
+             "right": "alt+right",
+             "top": "alt+up",
+             "bottom": "alt+down",
+             "top-left": "shift+alt+f1",
+             "bottom-left": "shift+alt+f2",
+             "top-right": "shift+alt+f3",
+             "bottom-right": "shift+alt+f4"
+             }
 
 def action_sleep():
     time.sleep(SLEEP_TIME)
@@ -13,7 +24,7 @@ def exit_with_error(error_msg):
     sys.exit(1)
 
 def move_mouse(x, y):
-    type_keys("xdotool mousemove %d %d" % (x, y))
+    run_command("xdotool mousemove %d, %d" % (x, y))
 
 def create_terminal():
     send_keys("ctrl+alt+t")
@@ -21,13 +32,28 @@ def create_terminal():
 def create_tab():
     send_keys("ctrl+shift+t")
 
-def send_keys():
-    # TODO
-    pass
+def send_keys(to_send):
+    run_command("xdotool key \"%s\"" % to_send)
 
-def type_keys():
-    # TODO
-    pass
+def type_in(to_type):
+    run_command("xdotool type %s" % to_type)
+
+def type_in_enter(to_type):
+    type_in(to_type)
+    send_keys("Return")
+
+def run_command(to_run):
+    split_command = to_run.split(" ")
+
+    assert len(split_command) >= 1
+
+    if len(split_command) > 1:
+        subprocess.call(to_run)
+    else:
+        subprocess.call(split_command)
+
+def snap_window(place):
+    assert place in SNAP_MAPS.keys()
 
 if __name__ == "__main__":
     # Verify that prepare script exists
